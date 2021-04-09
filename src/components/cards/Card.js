3ad736d1c5e8bb8ef5b96468b87110087a983192
Gizem <whiteswan1697@gmail.com>
2021-04-09 18:46:48 +0300

    Added key props to card component.

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -6,13 +6,13 @@ import "./Card.css"
 const Card = () => {
     return(
         <div className="card-container">
-            {data.map((card) => {
+            {data.map((card, index) => {
                 return (
-                    <div className="cards">
+                    <div className="cards" key={index}>
                         <div className="title">
                             <h2>{card.title}</h2>
                         </div>
-                        <img src={card.image}/>
+                        <img src={card.image} alt={card.title}/>
                         
                         <div className="card-over">
                             <p>{card.desc}</p>
